af5cee00ad640336b3f12e073e86c45158213ffb
Shelin Ayu Citra <shelinayucitra16@gmail.com>
2025-10-22 11:32:33 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="bg-brand-primary p-8 text-neutral-grey-blue">Hello</div>;
+  return <div className="bg-brand-primary p-8 text-neutral-grey-blue">AKU SHELIN!!</div>;
 }
