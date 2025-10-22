6808db803ff9a99291c79b845099b3dbfe213ae4
Shelin Ayu Citra <shelinayucitra16@gmail.com>
2025-10-22 12:08:57 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="bg-brand-primary p-8 text-neutral-grey-blue">AKU SHELIN!!</div>;
+  return <div className="bg-brand-primary p-8 text-neutral-grey-blue">slice/sheylnn</div>;
 }
---
+++ components/custom/header.tsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row bg-neutral-silver h-[84px] px-[156px] items-center">
+      <img src="./logo.png" className="w-[142px] h-[40px]" alt="" />
+
+      <div className="flex flex-row flex-1 justify-center items-center gap-8">
+        <p className="font-inter text-[16px]">Menu 1</p>
+        <p className="font-inter text-[16px]">Menu 2</p>
+        <p className="font-inter text-[16px]">Menu 3</p>
+        <p className="font-inter text-[16px]">Menu 4</p>
+        <p className="font-inter text-[16px]">Menu 5</p>
+        <p className="font-inter text-[16px]">Menu 6</p>
+      </div>
+
+      <div className="flex flex-row gap-4">
+        <button className="text-brand-primary text-[14px] font-inter w-[77px] h-[40px]">
+          Login
+        </button>
+        <button className="bg-brand-primary text-white text-[14px] font-inter w-[77px] h-[40px]">
+          Signup
+        </button>
+      </div>
+    </div>
+  );
+}
