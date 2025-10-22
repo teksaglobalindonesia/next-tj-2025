b933b285bc2d37d64f3f7a83a806dd152ca1e9ea
Shelin Ayu Citra <shelinayucitra16@gmail.com>
2025-10-22 13:45:31 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Header from "../components/custom/header";
+
 export default function Page() {
-  return <div className="bg-brand-primary p-8 text-neutral-grey-blue">slice/sheylnn</div>;
+  return (
+    <Header />
+  );
 }
--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -1,23 +1,21 @@
 export default function Header() {
   return (
-    <div className="flex flex-row bg-neutral-silver h-[84px] px-[156px] items-center">
-      <img src="./logo.png" className="w-[142px] h-[40px]" alt="" />
-
+    <div className="flex flex-row bg-neutral-silver h-[84px] px-[104px] items-center justify-between">
+      <img src="./logo.png" className="w-[154px] h-[24px] object-contain" alt="" />
       <div className="flex flex-row flex-1 justify-center items-center gap-8">
-        <p className="font-inter text-[16px]">Menu 1</p>
-        <p className="font-inter text-[16px]">Menu 2</p>
-        <p className="font-inter text-[16px]">Menu 3</p>
-        <p className="font-inter text-[16px]">Menu 4</p>
-        <p className="font-inter text-[16px]">Menu 5</p>
-        <p className="font-inter text-[16px]">Menu 6</p>
-      </div>
-
-      <div className="flex flex-row gap-4">
-        <button className="text-brand-primary text-[14px] font-inter w-[77px] h-[40px]">
+            <p className="font-inter text-[16px]">Home</p>
+            <p className="font-inter text-[16px]">Service</p>
+            <p className="font-inter text-[16px]">Feature</p>
+            <p className="font-inter text-[16px]">Product</p>
+            <p className="font-inter text-[16px]">Testimonial</p>
+            <p className="font-inter text-[16px]">FAQ</p>
+        </div>
+      <div className="flex flex-row gap-3 items-center">
+        <button className="text-brand-primary text-[14px] font-inter w-[91px] h-[40px] rounded-md hover:underline">
           Login
         </button>
-        <button className="bg-brand-primary text-white text-[14px] font-inter w-[77px] h-[40px]">
-          Signup
+        <button className="bg-brand-primary text-white text-[14px] font-inter w-[91px] h-[40px] rounded-md hover:opacity-90 transition-all duration-200">
+          Sign up
         </button>
       </div>
     </div>
