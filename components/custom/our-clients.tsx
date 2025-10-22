a362c85ad973935d81dc40d840e9576d27d71ed7
Shelin Ayu Citra <shelinayucitra16@gmail.com>
2025-10-22 16:42:28 +0800

    update

--- components/custom/our-clients.tsx
+++ components/custom/our-clients.tsx
@@ -1,6 +1,6 @@
 export default function Clients() {
   return (
-    <div className="flex flex-col items-center justify-center bg-white py-[60px]">
+    <div className="flex flex-col items-center justify-center bg-white py-[40px]">  
       <div className="text-center mb-10">
         <h2 className="text-[28px] font-bold text-gray-800">Our Clients</h2>
         <p className="text-gray-500 text-[16px] mt-2">
