ca73e56b3f3eb253f16ddb140ee4c0ae9d83a2fb
Kenzhinnn <kenzhinimpact@gmail.com>
2025-10-23 15:42:12 +0800

    Update | Service: Simple-Hero

---
+++ app/service/page.tsx
@@ -0,0 +1,12 @@
+
+import SimpleHero from "../../components/custom/simple-hero";
+import Header from "@/components/custom/header";
+
+export default function Page() {
+    return (
+       <main>
+        <Header />
+        <SimpleHero />
+       </main>
+    );  
+}
---
+++ components/custom/simple-hero.tsx
@@ -0,0 +1,8 @@
+export default function SimpleHero() {
+    return (
+       <div className="flex flex-col items-center justify-center bg-white px-[104px] py-[40px]">
+          <h2 className="text-neutral-dgrey text-[36px] font-inter text-center font-semibold">Our <span className="text-brand-primary">Service</span></h2>
+          <p className="font-inter text-neutral-grey text-[14px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed sit ametjusto ipsum.</p>
+       </div>
+    );
+}
