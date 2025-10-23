bdb5197110afdd42e8b9c3a47bf397e84cb0d8e9
natasyaa <deborawidana@gmail.com>
2025-10-23 16:00:20 +0800

    update spending

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "../components/custom/header";
 import Hero from "../components/custom/hero";
 import Clients from "../components/custom/our-clients";
 import Community from "../components/custom/community";
+import Spending from "../components/custom/unseen-spending";
 
 export default function Page() {
   return (
@@ -10,6 +11,7 @@ export default function Page() {
       <Hero />
       <Clients/>
       <Community />
+      <Spending />
 
     </main>
     
