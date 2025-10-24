9147fb9fc07363f02f6e19f8a0232a690cbcf493
Kenzhinnn <kenzhinimpact@gmail.com>
2025-10-24 10:41:00 +0800

    Update | Service: Service List

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,12 +1,14 @@
 
 import SimpleHero from "../../components/custom/simple-hero";
-import Header from "@/components/custom/header";
+import Header from "../../components/custom/header";
+import ServiceList from "../../components/custom/service-list";
 
 export default function Page() {
     return (
        <main>
         <Header />
         <SimpleHero />
+        <ServiceList />
        </main>
     );  
 }
