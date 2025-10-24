ff2e120733ded6f70fd8f6d43a17a7b00ce8cc0f
Shelin Ayu Citra <shelinayucitra16@gmail.com>
2025-10-24 13:51:21 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Achievement from "../components/custom/achievement";
 import Howtodesign from "../components/custom/how-to-design";
 import Marketing from "../components/custom/new-marketing";
 import Ribbon from "../components/custom/ribbon";
-import Footer from "../components/custom/footer";
+import Footer from "../components/custom/footer"; 
 
 export default function Page() {
   return (
---
+++ app/service/page.tsx
@@ -0,0 +1,9 @@
+import Header from "../../components/custom/header";
+
+export default function Page() {
+  return (
+    <main>
+        <Header />
+    </main>
+  );
+}
