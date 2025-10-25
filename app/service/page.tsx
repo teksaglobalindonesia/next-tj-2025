bb63143becc4403f0715b384000da9374e2ebe90
natasyaa <deborawidana@gmail.com>
2025-10-25 11:57:25 +0800

    update simple-hero

---
+++ app/service/page.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/custom/header";
+import Simple from "../../components/custom/simple-hero";
+
+
+export default function Page() {
+  return (
+    <main>
+      <Header />
+      <Simple />
+
+    </main>
+    
+  );
+}
---
+++ components/custom/simple-hero.tsx
@@ -0,0 +1,11 @@
+export default function Simple() {
+    return (
+        <section className="flex flex-col justify-center items-center bg-neutral-silver px-[104px] py-[88px] gap-[32px]">
+            <h1 className="text-[64px] text-neutral-dgrey font-inter font-semibold text-center w-[1152px] h-[76px]"> Our <span className="text-brand-primary">Service</span>
+            </h1>
+
+            <p className="text-[16px] text-neutral-grey font-inter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum.</p>
+
+        </section>
+    )
+}
